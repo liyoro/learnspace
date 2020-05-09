87e3489bac5b2d9393ebfa5f539f9b5fc40379c0
liyoro <liyoro@163.com>
2020-05-09 17:50:42 +0800

    first settings

---
+++ src/views/sass/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: liyoro
+ * @since: 2020-05-09 17:08:35
+ * @lastTime: 2020-05-09 17:42:54
+ -->
+<template>
+  <div class="sass-index">
+    fdasdfasdfs
+  </div>
+</template>
+
+<style scope lang="scss">
+  @import "./index.scss";
+</style>
+
+<script>
+import indexjs from './index.js'
+export default { ...indexjs }
+</script>
